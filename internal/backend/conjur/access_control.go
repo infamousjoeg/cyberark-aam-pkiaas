96436b6474465fa21c95ca1276b9443fecf20ca0
AndrewCopeland <andcope1995@gmail.com>
2020-06-17 16:19:38 -0400

    simplified the conjur access control. Each permission call only makes 1 request to the conjur api to check for permissions

--- internal/backend/conjur/access_control.go
+++ internal/backend/conjur/access_control.go
@@ -11,55 +11,16 @@ import (
 // Privileges ...
 type Privileges struct {
 	Authenticate              string
-	Admin                     string
-	Audit                     string
-	CertificateAdmin          string
-	TemplateAdmin             string
 	Purge                     string
 	CRLPurge                  string
-	CertificateSignAny        string
 	CertificateSignSpecific   string
-	CertificateCreateAny      string
 	CertificateCreateSpecific string
-	CertificateRevokeAny      string
 	CertificateRevokeSpecific string
 	TemplateCreateAny         string
-	TemplateManageAny         string
 	TemplateManageSpecific    string
-	TemplateDeleteAny         string
 	TemplateDeleteSpecific    string
-	TemplateReadAny           string
 	TemplateReadSpecific      string
-}
-
-// NewDefaultPrivileges ...
-func NewDefaultPrivileges() Privileges {
-	return Privileges{
-		Authenticate:              "use",
-		Admin:                     "admin",
-		Audit:                     "audit",
-		CertificateAdmin:          "certificate/admin",
-		TemplateAdmin:             "template/admin",
-		Purge:                     "purge",
-		CRLPurge:                  "crl/purge",
-		CertificateSignAny:        "certificate/sign",
-		CertificateSignSpecific:   "certificates/sign/{templateName}",
-		CertificateCreateAny:      "certificate/create",
-		CertificateCreateSpecific: "certificate/create/{templateName}",
-		CertificateRevokeAny:      "certificate/revoke",
-		CertificateRevokeSpecific: "certificate/revoke/{serialNumber}",
-		TemplateCreateAny:         "template/create",
-		TemplateManageAny:         "template/manage",
-		TemplateManageSpecific:    "template/manage/{templateName}",
-		TemplateDeleteAny:         "template/delete",
-		TemplateDeleteSpecific:    "template/delete/{templateName}",
-		TemplateReadAny:           "templates",
-		TemplateReadSpecific:      "template/{templateName}",
-	}
-}
-
-func replacePrivilege(privilege string, key string, value string) string {
-	return strings.ReplaceAll(privilege, fmt.Sprintf("{%s}", key), value)
+	ListTemplates             string
 }
 
 // AccessControl ...
@@ -68,6 +29,23 @@ type AccessControl struct {
 	policyBranch string
 	conjurConfig conjurapi.Config
 	disabled     bool
+}
+
+// NewDefaultPrivileges ...
+func NewDefaultPrivileges() Privileges {
+	return Privileges{
+		Authenticate:              "authenticate",
+		Purge:                     "purge",
+		CRLPurge:                  "purge-crl",
+		CertificateSignSpecific:   "sign-certificate-from-",
+		CertificateCreateSpecific: "create-certificate-from-",
+		CertificateRevokeSpecific: "revoke-certificate-",
+		TemplateCreateAny:         "create-templates",
+		TemplateManageSpecific:    "manage-template-",
+		TemplateDeleteSpecific:    "delete-template-",
+		TemplateReadSpecific:      "read-template-",
+		ListTemplates:             "list-templates",
+	}
 }
 
 // NewAccess ...
@@ -90,6 +68,10 @@ func NewAccessFromDefaultsDisabled(conjurConfig conjurapi.Config, policyBranch s
 	return NewAccess(conjurConfig, policyBranch, NewDefaultPrivileges(), true)
 }
 
+func replacePrivilege(privilege string, key string, value string) string {
+	return strings.ReplaceAll(privilege, fmt.Sprintf("{%s}", key), value)
+}
+
 func parseAccessToken(accessToken string) (string, error) {
 	accessToken = strings.ReplaceAll(accessToken, "Token token=\"", "")
 	accessToken = strings.Trim(accessToken, "\"")
@@ -98,153 +80,72 @@ func parseAccessToken(accessToken string) (string, error) {
 	return string(accessTokenSlice), err
 }
 
-// Authenticate ...
+// Authenticate If the client has ability to authenticate to the PKI service
 func (a AccessControl) Authenticate(accessToken string) error {
-	authenticated, err := a.checkPermission(accessToken, a.privileges.Authenticate)
-	if err != nil {
-		return fmt.Errorf("Failed to authenticate as client. %s", err)
-	}
-	if !authenticated {
-		return fmt.Errorf("You are not authenticated to use this PKI service")
-	}
-	return nil
+	return a.checkPermission(accessToken, a.privileges.Authenticate)
 }
 
 // ReadTemplates ...
-func (a AccessControl) ReadTemplates(accessToken string) error {
-	permissions := []string{
-		a.privileges.Admin,
-		a.privileges.Audit,
-		a.privileges.TemplateAdmin,
-		a.privileges.TemplateCreateAny,
-		a.privileges.TemplateManageAny,
-		a.privileges.TemplateReadAny,
-	}
-
-	return a.checkPermissions(accessToken, permissions)
+func (a AccessControl) ListTemplates(accessToken string) error {
+	return a.checkPermission(accessToken, a.privileges.ListTemplates)
 }
 
 // ReadTemplate ...
 func (a AccessControl) ReadTemplate(accessToken string, templateName string) error {
-	err := a.ReadTemplates(accessToken)
-	if err == nil {
-		return nil
-	}
-
-	permissions := []string{
-		replacePrivilege(a.privileges.TemplateReadSpecific, "templateName", templateName),
-	}
-
-	return a.checkPermissions(accessToken, permissions)
+	return a.checkPermission(accessToken, a.privileges.TemplateReadSpecific+templateName)
 }
 
 // DeleteTemplate ..
 func (a AccessControl) DeleteTemplate(accessToken string, templateName string) error {
-	permissions := []string{
-		a.privileges.Admin,
-		a.privileges.TemplateAdmin,
-		a.privileges.TemplateCreateAny,
-		a.privileges.TemplateDeleteAny,
-		replacePrivilege(a.privileges.TemplateDeleteSpecific, "templateName", templateName),
-	}
-
-	return a.checkPermissions(accessToken, permissions)
+	return a.checkPermission(accessToken, a.privileges.TemplateDeleteSpecific+templateName)
 }
 
 // ManageTemplate ...
 func (a AccessControl) ManageTemplate(accessToken string, templateName string) error {
-	permissions := []string{
-		a.privileges.Admin,
-		a.privileges.TemplateAdmin,
-		a.privileges.TemplateCreateAny,
-		a.privileges.TemplateManageAny,
-		replacePrivilege(a.privileges.TemplateManageSpecific, "templateName", templateName),
-	}
-
-	return a.checkPermissions(accessToken, permissions)
+	return a.checkPermission(accessToken, a.privileges.TemplateManageSpecific+templateName)
 }
 
-// CreateTemplate ...
+// CreateTemplate creating a template is not granular, you either have the ability to create templates or not
 func (a AccessControl) CreateTemplate(accessToken string) error {
-	permissions := []string{
-		a.privileges.Admin,
-		a.privileges.TemplateAdmin,
-		a.privileges.TemplateCreateAny,
-	}
-
-	return a.checkPermissions(accessToken, permissions)
+	return a.checkPermission(accessToken, a.privileges.TemplateCreateAny)
 }
 
 // Purge ...
 func (a AccessControl) Purge(accessToken string) error {
-	// Should Template admins or Cert admins also be able to purge?
-	permissions := []string{
-		a.privileges.Admin,
-		a.privileges.Purge,
-	}
-
-	return a.checkPermissions(accessToken, permissions)
+	return a.checkPermission(accessToken, a.privileges.Purge)
 }
 
 // CRLPurge ...
 func (a AccessControl) CRLPurge(accessToken string) error {
-	// Should Template admins or Cert admins also be able to purge?
-	permissions := []string{
-		a.privileges.Admin,
-		a.privileges.Purge,
-		a.privileges.CRLPurge,
-	}
-
-	return a.checkPermissions(accessToken, permissions)
+	return a.checkPermission(accessToken, a.privileges.CRLPurge)
 }
 
 // CreateCertificate ...
 func (a AccessControl) CreateCertificate(accessToken string, templateName string) error {
-	permissions := []string{
-		a.privileges.Admin,
-		a.privileges.CertificateAdmin,
-		a.privileges.CertificateCreateAny,
-		replacePrivilege(a.privileges.CertificateCreateSpecific, "templateName", templateName),
-	}
-
-	return a.checkPermissions(accessToken, permissions)
+	return a.checkPermission(accessToken, a.privileges.CertificateCreateSpecific+templateName)
 }
 
 // RevokeCertificate ...
 func (a AccessControl) RevokeCertificate(accessToken string, serialNumber string) error {
-	permissions := []string{
-		a.privileges.Admin,
-		a.privileges.CertificateAdmin,
-		a.privileges.CertificateCreateAny,
-		a.privileges.CertificateRevokeAny,
-		replacePrivilege(a.privileges.CertificateRevokeSpecific, "serialNumber", serialNumber),
-	}
-
-	return a.checkPermissions(accessToken, permissions)
+	return a.checkPermission(accessToken, a.privileges.CertificateRevokeSpecific+serialNumber)
 }
 
 // SignCertificate ...
 func (a AccessControl) SignCertificate(accessToken string, templateName string) error {
-	permissions := []string{
-		a.privileges.Admin,
-		a.privileges.CertificateAdmin,
-		a.privileges.CertificateSignAny,
-		replacePrivilege(a.privileges.CertificateSignSpecific, "templateName", templateName),
-	}
-
-	return a.checkPermissions(accessToken, permissions)
+	return a.checkPermission(accessToken, a.privileges.CertificateSignSpecific+templateName)
 }
 
 // AdminOnly ...
 func (a AccessControl) AdminOnly(accessToken string) error {
-	permissions := []string{
-		a.privileges.Admin,
-	}
-
-	return a.checkPermissions(accessToken, permissions)
+	return fmt.Errorf("Admin should no longer work!!!")
 }
 
-func (a AccessControl) checkPermissions(accessToken string, permissions []string) error {
+func (a AccessControl) checkPermissionWithPlaceholder(accessToken string, permission string, placeholder string, replaceValue string) error {
+	permission = replacePrivilege(permission, placeholder, replaceValue)
+	return a.checkPermission(accessToken, permission)
+}
+
+func (a AccessControl) checkPermission(accessToken string, permission string) error {
 	if a.disabled {
 		return nil
 	}
@@ -261,33 +162,15 @@ func (a AccessControl) checkPermissions(accessToken string, permissions []string
 	}
 
 	resourceID := fmt.Sprintf("%s:%s:%s", config.Account, "webservice", a.policyBranch)
-	for _, permission := range permissions {
-		privilege, _ := conjur.CheckPermission(resourceID, permission)
-		if privilege {
-			return nil
-		}
-	}
+	allowed, err := conjur.CheckPermission(resourceID, permission)
 
-	return fmt.Errorf("Privileges '%s' were not found on resource '%s'", strings.Join(permissions, ", "), resourceID)
-}
-
-func (a AccessControl) checkPermission(accessToken string, permission string) (bool, error) {
-	if a.disabled {
-		return true, nil
-	}
-
-	accessToken, err := parseAccessToken(accessToken)
 	if err != nil {
-		return false, fmt.Errorf("Failed to parse access token. %s", err)
+		return fmt.Errorf("Could not check the permissions. %s", err)
 	}
 
-	config := a.conjurConfig
-	conjur, err := conjurapi.NewClientFromToken(config, accessToken)
-	if err != nil {
-		return false, fmt.Errorf("Failed to init conjur client. %s", err)
+	if allowed {
+		return nil
+	} else {
+		return fmt.Errorf("You do not have the privilege '%s'", permission)
 	}
-
-	resourceID := fmt.Sprintf("%s:%s:%s", config.Account, "webservice", a.policyBranch)
-	allowed, err := conjur.CheckPermission(resourceID, a.privileges.Authenticate)
-	return allowed, err
 }
