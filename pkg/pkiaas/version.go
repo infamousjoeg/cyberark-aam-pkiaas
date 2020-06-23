91760f4b89b9a972cc1fd95f6156e8d1911f017d
Rob <robert.cobbins@cyberark.com>
2020-06-23 10:40:22 -0400

    Update version.go

--- pkg/pkiaas/version.go
+++ pkg/pkiaas/version.go
@@ -4,11 +4,8 @@ import "fmt"
 
 // Version field is a SemVer that should indicate the baked-in version
 // of the broker
-<<<<<<< HEAD
-var Version = "0.4.3"
-=======
-var Version = "0.4.2"
->>>>>>> Added panic for invalid initialization
+
+var Version = "0.5.0"
 
 // Tag field denotes the specific build type for the broker. It may
 // be replaced by compile-time variables if needed to provide the git
