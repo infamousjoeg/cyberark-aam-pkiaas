3b4c944c5866fbda16fcd73096175bfce41f02e3
rcobbins <r.b.cobbins@gmail.com>
2020-07-06 11:56:24 -0400

    Added comments to new functions

--- internal/pki/ca_service.go
+++ internal/pki/ca_service.go
@@ -110,6 +110,8 @@ func GenerateIntermediate(intermediateRequest types.IntermediateRequest, selfSig
 	return intermediateResponse, httpErr
 }
 
+// CreateIntermediateCSR Generates a CSR used for the intermediate signing CA and returns
+// HTTPError if it fails
 func CreateIntermediateCSR(signRequest x509.CertificateRequest, signPrivKey crypto.PrivateKey) (types.PEMIntermediate, httperror.HTTPError) {
 	// Set X.509 extensions to specify that this CSR is for a CA certificate
 	extraExtensions := []pkix.Extension{}
@@ -161,6 +163,8 @@ func CreateIntermediateCSR(signRequest x509.CertificateRequest, signPrivKey cryp
 	return types.PEMIntermediate{CSR: string(pemSignCSR)}, httperror.HTTPError{}
 }
 
+// CreateSelfSignedCert Generates a self signed CA certificate for the PKI service and returns
+// HTTPError if it fails
 func CreateSelfSignedCert(certTemplate x509.Certificate, signPrivKey crypto.PrivateKey, signPubKey crypto.PublicKey, backend backend.Storage) (types.PEMIntermediate, httperror.HTTPError) {
 	serialNumber, err := GenerateSerialNumber(backend)
 	if err != nil {
