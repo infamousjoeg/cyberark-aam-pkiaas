ba2badd37193328fa24761ecc7000c7b487b5ef8
rcobbins <r.b.cobbins@gmail.com>
2020-07-06 11:48:43 -0400

    Fix Issue #60; add self signed as independent endpoint

--- internal/pki/ca_service.go
+++ internal/pki/ca_service.go
@@ -109,8 +109,14 @@ func GenerateIntermediate(intermediateRequest types.IntermediateRequest, selfSig
 	return intermediateResponse, httpErr
 }
 
+<<<<<<< HEAD
 // CreateIntermediateCSR Generates a CSR used for the intermediate signing CA and returns
 // HTTPError if it fails
+=======
+	return intermediateResponse, httpErr
+}
+
+>>>>>>> Fix Issue #60; add self signed as independent endpoint
 func CreateIntermediateCSR(signRequest x509.CertificateRequest, signPrivKey crypto.PrivateKey) (types.PEMIntermediate, httperror.HTTPError) {
 	// Set X.509 extensions to specify that this CSR is for a CA certificate
 	extraExtensions := []pkix.Extension{}
@@ -162,8 +168,11 @@ func CreateIntermediateCSR(signRequest x509.CertificateRequest, signPrivKey cryp
 	return types.PEMIntermediate{CSR: string(pemSignCSR)}, httperror.HTTPError{}
 }
 
+<<<<<<< HEAD
 // CreateSelfSignedCert Generates a self signed CA certificate for the PKI service and returns
 // HTTPError if it fails
+=======
+>>>>>>> Fix Issue #60; add self signed as independent endpoint
 func CreateSelfSignedCert(certTemplate x509.Certificate, signPrivKey crypto.PrivateKey, signPubKey crypto.PublicKey, backend backend.Storage) (types.PEMIntermediate, httperror.HTTPError) {
 	serialNumber, err := GenerateSerialNumber(backend)
 	if err != nil {
