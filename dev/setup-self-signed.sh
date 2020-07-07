cd2aaeb9184194731aa20270accaf843a5617878
Joe Garcia <infamousjoeg@users.noreply.github.com>
2020-07-07 13:41:55 -0400

    Updated dev environment to Docker-Compose (#81)
    * uncommented initconfig AGAIN
    
    * fixed typo
    
    * Dockerfile.dev added
    
    * use the new self signed endpoint
    
    * Update README to reflect instructions for new env
    
    Co-authored-by: AndrewCopeland <andcope1995@gmail.com>

--- dev/setup-self-signed.sh
+++ dev/setup-self-signed.sh
@@ -9,7 +9,7 @@ export VERBOSE=""
 
 source conjur_utils.sh
 session_token=$(conjur_authenticate)
-pki_url="http://localhost:8080"
+pki_url="http://pkiaas:8080"
 
 export session_token="$session_token"
 
@@ -17,14 +17,13 @@ export session_token="$session_token"
 data='{
   "commonName": "cyberark.pki.local",
   "keyAlgo": "RSA",
-  "keyBits": "2048",
-  "selfSigned": true
+  "keyBits": "2048"
 }'
 curl --fail -H "Content-Type: application/json" \
   -H "$session_token" \
   --data "$data" \
   $VERBOSE \
-  $pki_url/ca/generate
+  $pki_url/ca/generate/selfsigned
 
 # create a test template
 data='{
