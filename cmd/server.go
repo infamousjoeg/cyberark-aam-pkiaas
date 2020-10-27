bf384a9eb63ba97c769d0242295ce1192a324f86
AndrewCopeland <andcope1995@gmail.com>
2020-10-27 15:12:56 -0400

    Add TLS and generate cert from pki-service template

--- cmd/server.go
+++ cmd/server.go
@@ -1,22 +1,27 @@
 package cmd
 
 import (
-	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/infamousjoeg/cyberark-aam-pkiaas/internal/backend"
+	"github.com/infamousjoeg/cyberark-aam-pkiaas/internal/backend/conjur"
+	"github.com/infamousjoeg/cyberark-aam-pkiaas/internal/backend/vault"
 	"github.com/infamousjoeg/cyberark-aam-pkiaas/internal/httperror"
 	"github.com/infamousjoeg/cyberark-aam-pkiaas/internal/pki"
 	"github.com/infamousjoeg/cyberark-aam-pkiaas/internal/types"
+	"github.com/infamousjoeg/cyberark-aam-pkiaas/pkg/log"
 
 	"github.com/spf13/cobra"
 )
 
 var storage backend.Storage
 
-var cert string = "/etc/ssl/server.pem"
-var key string = "/etc/ssl/server.key"
+const (
+	certFile = "/etc/ssl/server.pem"
+	keyFile  = "/etc/ssl/server.key"
+)
 
 // Gets the port number supplied by end-user or else defaults to 443
 func getPort() string {
@@ -27,46 +32,58 @@ func getPort() string {
 	return ":443"
 }
 
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
 func createServerTLS() {
-	// If cert and key exists, just return
-	if _, err := os.Stat("/etc/ssl/server.key"); err == nil {
+	// If file already exists use them
+	if fileExists(certFile) && fileExists(keyFile) {
 		return
-	} else if os.IsNotExist(err) {
-		certReq := types.CreateCertReq{
-			TemplateName: "pki-service",
-			CommonName:   CommonName,
-			AltNames:     AltNames,
-		}
+	}
 
-		certResp, httpErr := pki.CreateCert(certReq, storage)
-		if httpErr != (httperror.HTTPError{}) {
-			httpErr.JSON()
-			os.Exit(1)
-		}
+	log.Info("Generating server certificate from template 'pki-service'")
 
-		pemCert := certResp.Certificate
-		pemPrivKey := certResp.PrivateKey
-		certFile, err := os.Create(cert)
-		if err != nil {
-			log.Fatal("Error creating server TLS self-signed certificate file: " + err.Error())
-		}
+	certReq := types.CreateCertReq{
+		TemplateName: "pki-service",
+		CommonName:   CommonName,
+		AltNames:     AltNames,
+	}
 
-		_, err = certFile.Write([]byte(pemCert))
-		if err != nil {
-			log.Fatal("Error writing to server TLS self-signed certificate file: " + err.Error())
-		}
+	certResp, httpErr := pki.CreateCert(certReq, storage)
+	if httpErr != (httperror.HTTPError{}) {
+		httpErr.JSON()
+		os.Exit(1)
+	}
 
-		keyFile, err := os.Create(key)
-		if err != nil {
-			log.Fatal("Error creating server private key file: " + err.Error())
-		}
+	pemCert := certResp.Certificate
+	pemPrivKey := certResp.PrivateKey
+	certFile, err := os.Create(certFile)
+	if err != nil {
+		log.Error("Error creating server TLS certificate file: " + err.Error())
+	}
 
-		_, err = keyFile.Write([]byte(pemPrivKey))
-		if err != nil {
-			log.Fatal("Error writing to server private key file: " + err.Error())
-		}
-	} else {
-		log.Fatal("Unknown error reading /etc/ssl directory.")
+	_, err = certFile.Write([]byte(pemCert))
+	if err != nil {
+		log.Error("Error writing to server TLS certificate file: " + err.Error())
+	}
+
+	keyFile, err := os.Create(keyFile)
+	if err != nil {
+		log.Error("Error creating server private key file: " + err.Error())
+	}
+
+	_, err = keyFile.Write([]byte(pemPrivKey))
+	if err != nil {
+		log.Error("Error writing to server private key file: " + err.Error())
+	}
+
+	if err != nil {
+		os.Exit(1)
 	}
 }
 
@@ -78,12 +95,27 @@ var serverCMD = &cobra.Command{
 	Example Usage:
 	$ pkiaas server -c pki1-service.company.local -a pki-altname.company.local`,
 	Run: func(cmd *cobra.Command, args []string) {
+		vaultBackend := strings.ToLower(os.Getenv("PKI_VAULT_BACKEND"))
+
+		var err error
+		if vaultBackend == "yes" || vaultBackend == "true" {
+			storage, err = vault.NewFromDefaults()
+		} else {
+			storage, err = conjur.NewFromDefaults()
+		}
+
+		if err != nil {
+			log.Error("Error initializing PKI backend. %s", err.Error())
+			os.Exit(1)
+		}
+
 		createServerTLS()
 
-		log.Printf("Server started")
+		log.Info("Server started")
 		router := NewRouter()
 		port := getPort()
-		log.Fatal(http.ListenAndServe(port, router))
+		log.Error("%s", http.ListenAndServeTLS(port, certFile, keyFile, router))
+		os.Exit(1)
 	},
 }
 
